989e509513d5770085655a726feb3da126576b18
Nicolas Terray <nicolas.terray@enalean.com>
2018-03-09 11:52:06 +0100

    Add tests for Mock::generate/generatePartial
    This cover the transformation of new MockFoo (cf Mock::generate), new
    FooTestVersion (cf Mock::generatePartial), mock(), and partial_mock();

---
+++ tests/_expected/MockTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'bootstrap.php';
+
+class MockTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->tracker = \Mockery::spy(Foo::class);
+    }
+
+    public function testMethodsAreConverted()
+    {
+        $foo = \Mockery::spy(Foo::class);
+        $foo_getTrackerById = $foo->shouldReceive('getTrackerById');
+        $foo_getTrackerById->with('param1', 'param2');
+        $foo_getTrackerById->andReturns('result');
+    }
+
+    public function itConvertsAlsoItMethods()
+    {
+        $foo = \Mockery::spy(Foo::class);
+        $foo_searchAncestorIds = $foo->shouldReceive('searchAncestorIds');
+        $foo_searchAncestorIds->with('param1');
+        $foo_searchAncestorIds->once();
+        $foo_searchAncestorIds->andReturns('result');
+    }
+
+    public function testWhithOnceLaterInTheStack()
+    {
+        $foo = \Mockery::spy(Foo::class);
+        $foo_searchAncestorIds = $foo->shouldReceive('searchAncestorIds');
+        $foo_searchAncestorIds->andReturns('result');
+
+        buildOtherStuff();
+        $foo_searchAncestorIds->with('param1');
+
+        $foo_searchAncestorIds->once();
+    }
+
+    public function testWhithMockHelper()
+    {
+        $bar = \Mockery::spy(Barbell\Bar::class);
+    }
+
+    public function testPartialMock()
+    {
+        $baz = \Mockery::mock(Baz::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $baz_meth1 = $baz->shouldReceive('meth1');
+        $baz_meth1->once();
+    }
+
+    public function testWithPartialMock()
+    {
+        $baz = \Mockery::mock(Food\Truck::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $baz_burger = $baz->shouldReceive('burger');
+        $baz_burger->once();
+    }
+
+    public function testWhenInitialisationIsDoneInSetUp()
+    {
+        $this_tracker_getId = $this->tracker->shouldReceive('getId');
+        $this_tracker_getId->andReturns(123);
+    }
+}
---
+++ tests/_fixtures/MockTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'bootstrap.php';
+Mock::generate('Foo');
+Mock::generatePartial('Baz', 'BazTestVersion', array('meth1',
+    'meth2'));
+
+class MockTest
+{
+    public function setUp()
+    {
+        $this->tracker = new MockFoo();
+    }
+
+    public function testMethodsAreConverted()
+    {
+        $foo = new MockFoo();
+        $foo->setReturnValue('getTrackerById', 'result', array('param1', 'param2'));
+    }
+
+    public function itConvertsAlsoItMethods()
+    {
+        $foo = new MockFoo();
+        $foo->expectOnce('searchAncestorIds', array('param1'));
+        $foo->setReturnValue('searchAncestorIds', 'result');
+    }
+
+    public function testWhithOnceLaterInTheStack()
+    {
+        $foo = new MockFoo();
+        $foo->setReturnValue('searchAncestorIds', 'result');
+
+        buildOtherStuff();
+
+        $foo->expectOnce('searchAncestorIds', array('param1'));
+    }
+
+    public function testWhithMockHelper()
+    {
+        $bar = mock('Barbell\Bar');
+    }
+
+    public function testPartialMock()
+    {
+        $baz = new BazTestVersion();
+        $baz->expectOnce('meth1');
+    }
+
+    public function testWithPartialMock()
+    {
+        $baz = partial_mock('Food\Truck', ['burger'], ['constructor_param1', 'constructor_param2']);
+        $baz->expectOnce('burger');
+    }
+
+    public function testWhenInitialisationIsDoneInSetUp()
+    {
+        $this->tracker->setReturnValue('getId', 123);
+    }
+}
